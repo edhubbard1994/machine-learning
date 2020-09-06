768af9b14fe236cca2da57c73debacb5a87c681b
Ed Hubbard <Edhubbard1994@gmail.com>
2020-09-06 18:51:25 -0500

    load survivers

--- HW_1/prediction.py
+++ HW_1/prediction.py
@@ -2,8 +2,10 @@ import csv
 import numpy
 
 #MATRIX = numpy.array()
+SURVIVERS = dict()
 FARE_INDEX = 8
 PID_INDEX = 0
+FARES = list()
 
 def load_data():
     with open("titanic/test.csv") as file:
@@ -14,8 +16,19 @@ def load_data():
 
 
 
+def load_survivers():
+    with open("titanic/gender_submission.csv") as file:
+        data = csv.reader(file)
+        for row in data:
+            SURVIVERS[row[0]] = row[1]
+        print(SURVIVERS)
+
+
+
+
+
 
 
 
 if __name__ == "__main__":
-    load_data()
+    load_survivers()
